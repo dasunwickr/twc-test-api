23e013c405f1ff2cf7135ebca69f4c2fbdf47a10
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-06-13 12:12:51 +0530

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ContactModule } from './contact/contact.module';
+import { PassportModule } from '@nestjs/passport';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ContactModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    ContactModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
