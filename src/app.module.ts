76d39598080733be4af6e73eab61f170d94ae46f
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-06-13 10:29:49 +0530

    Init Dev Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UserModule, ContactModule],
   controllers: [],
   providers: [],
 })
