ef73da64e503827cf667cbc35c3212c8d9d08cf2
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-06-20 17:14:17 +0530

    cors configs

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,5 +1,5 @@
 export const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174'], // Frontend URLs
     methods: 'GET,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
   };
