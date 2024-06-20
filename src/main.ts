61a5cf3011e9803563ba6b53a27b716121a08b40
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-06-20 17:13:55 +0530

    cors config revision

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { corsOptions } from './config/corsOptions';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+
+  // CORS config - ./src/config/corsOptions.ts
   app.enableCors(corsOptions);
 
   await app.listen(3000);
