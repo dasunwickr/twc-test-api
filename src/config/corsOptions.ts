f422aa3db810931fe6279f24033a56086537cac0
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-06-20 17:12:23 +0530

    CORS fix

---
+++ src/config/corsOptions.ts
@@ -0,0 +1,6 @@
+export const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    methods: 'GET,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { corsOptions } from './config/corsOptions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'http://localhost:5173', 
-    methods: 'GET,PATCH,POST,DELETE', 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-  });
+  app.enableCors(corsOptions);
 
   await app.listen(3000);
 }
